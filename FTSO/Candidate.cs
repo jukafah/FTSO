4105c2d014ea6d59c4d5f07a6c59402c8c218305
Steve Momcilovic <smomcilovic@tapqa.com>
2017-07-30 14:28:20 -0500

    extract implementation

--- FTSO/Candidate.cs
+++ FTSO/Candidate.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
 
 namespace FTSO
 {
     public class Candidate
     {
+        private const string SkillWeight = "skillWeight";
+        private const string MinimumSkill = "minimumSkill";
+        
         private string candidateName;
         private string interviewedBy;
         
@@ -11,6 +17,49 @@ namespace FTSO
         {
             this.candidateName = candidateName;
             this.interviewedBy = interviewedBy;
+        }
+
+
+        public void GiveFeedback()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            
+            StreamReader skillStreamReader = new StreamReader(
+                assembly.GetManifestResourceStream("FTSO.Resources.skills.json")
+            );
+            JObject skills = JObject.Parse(skillStreamReader.ReadToEnd());
+
+            StreamReader roleStreamReader =
+                new StreamReader(assembly.GetManifestResourceStream("FTSO.Resources.roles.json")
+                );
+            JObject roles = JObject.Parse(roleStreamReader.ReadToEnd());
+           
+            
+            
+            try
+            {
+                foreach (var skill in skills)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Key: {skill.Key}");
+                    Console.WriteLine($"Skill Weight: {(int) skill.Value.SelectToken(SkillWeight)}");
+                    Console.WriteLine();
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("poopfish");
+                throw;
+            }
+
+
+            foreach (var role in roles)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"role key: {role.Key}");
+                Console.WriteLine($"Minimum Skill Level for role: {(int)role.Value.SelectToken(MinimumSkill)}");
+                Console.WriteLine();
+            }
         }
     }
 }
--- FTSO/Program.cs
+++ FTSO/Program.cs
@@ -9,24 +9,11 @@ namespace FTSO
 {
     internal class Program
     {
-        private const string SkillWeight = "skillWeight";
-        private const string MinimumSkill = "minimumSkill";
-//        private static Candidate Candidate;
-        
+
+
         public static void Main(string[] args)
         {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            
-            StreamReader skillStreamReader = new StreamReader(
-                assembly.GetManifestResourceStream("FTSO.Resources.skills.json")
-            );
-            JObject skills = JObject.Parse(skillStreamReader.ReadToEnd());
 
-            StreamReader roleStreamReader =
-                new StreamReader(assembly.GetManifestResourceStream("FTSO.Resources.roles.json")
-            );
-            JObject roles = JObject.Parse(roleStreamReader.ReadToEnd());
-           
             
             Console.Write("Candidate Name: ");
             string candidateName = Console.ReadLine();
@@ -39,32 +26,8 @@ namespace FTSO
 
             
             // extract to candidate implementation
-            //candidate.GiveFeedBack();
+            candidate.GiveFeedback();
 
-            try
-            {
-                foreach (var skill in skills)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine($"Key: {skill.Key}");
-                    Console.WriteLine($"Skill Weight: {(int) skill.Value.SelectToken(SkillWeight)}");
-                    Console.WriteLine();
-                }
-            }
-            catch (ArgumentNullException ex)
-            {
-                Console.WriteLine("poopfish");
-                throw;
-            }
-
-
-            foreach (var role in roles)
-            {
-                Console.WriteLine();
-                Console.WriteLine($"role key: {role.Key}");
-                Console.WriteLine($"Minimum Skill Level for role: {(int)role.Value.SelectToken(MinimumSkill)}");
-                Console.WriteLine();
-            }
         }
     }
 }
