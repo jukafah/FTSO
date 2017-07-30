0bade4ccb3477f8196afaf328a961cc2e78a0903
Steve Momcilovic <smomcilovic@tapqa.com>
2017-07-30 14:02:37 -0500

    .gitignore

---
+++ FTSO/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+
+namespace FTSO
+{
+    internal class Program
+    {
+        private const string SkillWeight = "skillWeight";
+        private const string MinimumSkill = "minimumSkill";
+        
+        public static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            
+            StreamReader skillStreamReader = new StreamReader(
+                assembly.GetManifestResourceStream("FTSO.Resources.skills.json")
+            );
+            JObject skills = JObject.Parse(skillStreamReader.ReadToEnd());
+
+            StreamReader roleStreamReader =
+                new StreamReader(assembly.GetManifestResourceStream("FTSO.Resources.roles.json")
+            );
+            JObject roles = JObject.Parse(roleStreamReader.ReadToEnd());
+           
+            
+            Console.WriteLine("Candidate Name");
+
+           
+
+
+//            Console.WriteLine("Candidate Name");
+//            string nameOfCandidate = Console.ReadLine();
+//            
+//            Console.WriteLine("Interviewers");
+//            List<String> interviewers = Console.ReadLine().Split(',').ToList();
+//            
+//            Console.WriteLine();
+//            Console.WriteLine($"The candidate was: {nameOfCandidate}");
+//            Console.WriteLine();
+//            Console.WriteLine($"And was interviewed by: {interviewers}");
+
+
+            try
+            {
+                foreach (var skill in skills)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Key: {skill.Key}");
+//                Console.WriteLine($"Skill Weight: {(int)skill.Value.SelectToken("weight")}");
+                    Console.WriteLine($"Skill Weight: {(int) skill.Value.SelectToken(SkillWeight)}");
+                    Console.WriteLine();
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("poopfish");
+                throw;
+            }
+
+
+            foreach (var role in roles)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"role key: {role.Key}");
+                Console.WriteLine($"Minimum Skill Level for role: {(int)role.Value.SelectToken(MinimumSkill)}");
+                Console.WriteLine();
+            }
+
+//            Console.WriteLine("Candidate Name:");
+//            string input = Console.ReadLine();
+//            
+//            Console.Write($"Yout typed {input}");
+//            Console.WriteLine();
+//            Console.WriteLine();
+        }
+    }
+}
---
+++ FTSO/README.md
@@ -0,0 +1,60 @@
+INPUTS
+
+General Inputs
+	Name
+	Interviewers
+	Giver of feedback
+
+	Initial role interviewed for (say resume reflects mid-level)
+
+Specific Inputs
+
+	Listed ( and relevant ) programming languages
+	Test tools ( selenium, appium, ranorex, etc)
+	Test frameworks ( junit, cucumber, nunit, etc)
+	SDLC processes
+	
+	other shit
+	
+	
+	
+	
+	
+Given General inputs
+	Interviewee : fuckhead
+	Interviewers : steve, zach
+	FeedbackGiver : Zach
+
+Given the following inputs for test frameworks:
+	Nunit, Xunit, MsTest
+	
+Given the following languages for languages:
+	C#, Powershell, Java
+	
+---------------------------------------
+
+The program would then create a structure that looked something like this:
+
+---------------------------------------
+	
+Interviewee : Head, Fuck
+Interviewers : Zach, Steve
+Report written by: Zach
+
+General:
+
+//Filled out after template is generated
+
+Skills
+
+Test Frameworks
+Role: // after the fact, write associate level if they have no experince w/ this, etc
+Tools: nUnit, msTest, xUnit
+Summary:
+	// after generated, write a quick summary of answers related to tools
+
+Programming
+Role: // associate, junior, etc
+Tools: C#, Java, Powershell
+Summary:
+	// talk about their programming abilities, and which of these languages are strongest
